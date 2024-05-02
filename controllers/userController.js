22e7bb6dc04af4ad495c97befddbe3317f5aeb01
amitsingh771087 <amitsingh77108792@gmail.com>
2024-05-02 14:56:15 +0530

    remove bcrypt from backend

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,93 +1,88 @@
 import userModel from "../models/userModel.js";
 import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
-import validator from 'validator'
+import crypto from 'crypto';
+import validator from 'validator';
 
 // login user
 
-const loginUser = async (req, res)=>{
-
-    const {email, password} = req.body;
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
 
     try {
-        const user = await userModel.findOne({email})
-        if(!user){
-          return  res.json({success:false, message:"user Doen't Exist"})
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            return res.json({ success: false, message: "User Doesn't Exist" });
         }
-        const isMatch = await bcrypt.compare(password, user.password);
 
-        if(!isMatch){
-          return  res.json({success:false, message:"Invalid credintials"})
+        const hashedPassword = hashPassword(password);
+        if (user.password !== hashedPassword) {
+            return res.json({ success: false, message: "Invalid credentials" });
         }
+
         const token = createToken(user._id);
-        res.json({success:true,token})
-        
+        res.json({ success: true, token });
+
     } catch (error) {
         console.log(error);
-        res.json({success:false, message:"Error"})
+        res.json({ success: false, message: "Error" });
     }
-
-    
-
 }
 
-const createToken = (id)=>{
-    return jwt.sign({id}, process.env.JWT_SECRET)
+const createToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET);
+}
+
+const hashPassword = (password) => {
+    return crypto.createHash('sha256').update(password).digest('hex');
 }
 
 // register user
 
-const registerUser = async (req, res)=>{
-    const {name,password, email } = req.body;
+const registerUser = async (req, res) => {
+    const { name, password, email } = req.body;
 
     try {
-        // checking is user already exist
-        const exist = await userModel.findOne({email});
-        if(exist){
+        // checking if user already exists
+        const exist = await userModel.findOne({ email });
+        if (exist) {
             return res.json({
-                success:false,
-                message:"User already exist"
-            })
-
+                success: false,
+                message: "User already exists"
+            });
         }
+
         // validation email format and strong password
 
-        if(!validator.isEmail(email)){
+        if (!validator.isEmail(email)) {
             return res.json({
-                success:false, message:"please enter a valid email"
-            })
+                success: false, message: "Please enter a valid email"
+            });
         }
-        if(password.length<8 ){
+        if (password.length < 8) {
             return res.json({
-                success:false,
-                message:"please enter a strong password"
-            })
+                success: false,
+                message: "Please enter a strong password"
+            });
         }
+
         // hashing user password
-        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = hashPassword(password);
 
-        const hashedpassword = await bcrypt.hash(password, salt);
+        const newUser = new userModel({
+            name: name,
+            email: email,
+            password: hashedPassword,
+        });
 
-        const newUser = new userModel(
-            {
-                name:name,
-                email: email,
-                password: hashedpassword,
-            }
-        )
-
-      const user =   await newUser.save()
-      const token =  createToken(user._id);
-      res.json({success:true , token})
-
-    
+        const user = await newUser.save();
+        const token = createToken(user._id);
+        res.json({ success: true, token });
 
     } catch (error) {
         console.log(error)
-        res.json({success:false , message:"Error"})
-        
-    }
+        res.json({ success: false, message: "Error" });
 
+    }
 }
 
-export {loginUser, registerUser}
+export { loginUser, registerUser };
