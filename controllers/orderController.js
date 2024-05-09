0941201066c66143044004c25493a0a7914eb276
amitsingh771087 <amitsingh77108792@gmail.com>
2024-05-09 20:55:04 +0530

    controllers/orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 // placing order from frontend
 const placeOrder = async (req , res)=>{
 
-    const frontend_url = 'https://food-del-backend.onrender.com/'
+    const frontend_url = 'https://food-del-backend.onrender.com'
     
 
     try{
